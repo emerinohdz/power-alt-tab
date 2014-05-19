119d820007b36c6fc68fb4026c361fb8d90edbc1
Edgar Merino <emerino@nuevebit.com>
2014-05-18 20:14:27 -0500

    added default watch task, when extension source changes extension is reinstalled locally and gnome shell is restarted ; added tasks to enable/disable/uninstall extension

--- gulpfile.js
+++ gulpfile.js
@@ -9,19 +9,31 @@
 var fs = require('fs');
 var path = require('path');
 var zip = require('gulp-zip');
+var spawn = require('child_process').spawn;
+var exec = require('child_process').exec;
 
 // gulp plugins
 var gulp = require("gulp");
 var clean = require('gulp-clean');
 
+//extension metadata
+var metadata = JSON.parse(fs.readFileSync("src/metadata.json"));
+
 // local config
 var config = {
     srcDir: path.join(__dirname, "src"),
-    distDir: path.join(__dirname, "dist")
+    distDir: path.join(__dirname, "dist"),
+    installDir: path.join(process.env.HOME, ".local/share/gnome-shell/extensions/" + metadata.uuid)
 };
 
-//extension metadata
-var metadata = JSON.parse(fs.readFileSync("src/metadata.json"));
+var enableExtension = function(enable, cb) {
+    var option = enable ? "-e" : "-d";
+    spawn("gnome-shell-extension-tool", [option, metadata.uuid], {stdio: "inherit"})
+            .on("exit", function() {
+                cb();
+            });
+};
+
 
 /**
  * Clean dist dir
@@ -35,17 +47,69 @@ gulp.task("clean", function() {
  * Create ZIP file for distribution to gse
  */
 gulp.task("dist", function() {
-    return gulp.src(config.srcDir + "/*")
+    return gulp.src(config.srcDir + "/**/*")
             .pipe(zip(metadata.uuid + ".zip"))
             .pipe(gulp.dest(config.distDir));
 });
 
 /**
+ * Copy the extension to local extensions folder only
+ */
+gulp.task("copy:extension", function() {
+    return gulp.src(config.srcDir + "/**/*")
+            .pipe(gulp.dest(config.installDir));
+});
+
+/**
+ * Enable extension.
+ */
+gulp.task("enable", function(cb) {
+    enableExtension(true, cb);
+});
+
+/**
+ * Disable extension.
+ */
+gulp.task("disable", function(cb) {
+    enableExtension(false, cb);
+});
+
+/**
  * Install extension locally.
  */
-gulp.task("install", function() {
-    var dest = path.join(process.env.HOME, ".local/share/gnome-shell/extensions/" + metadata.uuid);
+gulp.task("install", ["copy:extension"], function(cb) {
+    return gulp.start("enable");
+});
 
-    return gulp.src(config.srcDir + "/*")
-            .pipe(gulp.dest(dest));
+/**
+ * Uninstall extension locally. Removes install dir.
+ */
+gulp.task("uninstall", ["disable"], function(cb) {
+    return gulp.src(config.installDir).pipe(clean({force: true}));
+});
+
+
+/**
+ * Restart gnome shell task.
+ */
+gulp.task("restart:gnome-shell", ["copy:extension"], function() {
+    var out = fs.openSync('./out.log', 'a');
+    var err = fs.openSync('./out.log', 'a');
+    var gs = spawn('gnome-shell', ["-r"], { detached: true });
+    gs.stdout.on("data", function(chunk) {
+        process.stdout.write(chunk.toString());
+    });
+    gs.stderr.on("data", function(chunk) {
+        process.stdout.write(chunk.toString());
+    });
+
+    gs.unref();
+
+});
+
+/**
+ * Watch files for changes and reinstall then restart gs
+ */
+gulp.task("default", ["copy:extension"],  function() {
+    gulp.watch([config.srcDir + "/**/*"], ["restart:gnome-shell"]);
 });
