f95407c63727ae2525570719a3c34aa0a57ddc58
Edgar Merino <emerino@nuevebit.com>
2017-12-04 12:36:57 -0600

    removed unused babel preset

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,6 @@ module.exports = {
                     options: {
                         presets: [
                             require('babel-preset-env'),
-                            require('babel-preset-es2017'),
                         ],
                         plugins: [
                             require('babel-plugin-transform-runtime'),
